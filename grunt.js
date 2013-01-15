9f388d969c493b9001ac7a96cca3fcbcd47973cf
lexich <lexich121@gmail.com>
2013-01-15 23:45:48 +0400

    add default task + cp + connect

--- grunt.js
+++ grunt.js
@@ -1,16 +1,36 @@
 module.exports = function (grunt) {
 
-	grunt.initConfig({
-		jade: {
-			html: {
-				src: ['templates/*.jade'],
-				dest: 'www',
-				options: {	      
-					client:false
-				}
-			}
-		}
-	});
+  grunt.initConfig({
+    jade: {
+      html: {
+        src: ['templates/*.jade'],
+        dest: 'www',
+        options: {        
+          client:false
+        }
+      }
+    },
+    cp: {
+      media: {
+        src: 'static',
+        dest: 'www/static'
+      }
+    },
+    clean:{
+      folder:"www"
+    },
+    connect: {
+      server: {
+        port: 8080,
+        base: './www'
+      }
+    }
 
-	grunt.loadNpmTasks('grunt-jade');
+  });
+  grunt.registerTask('default',"clean jade cp");
+
+  grunt.loadNpmTasks('grunt-jade');
+  grunt.loadNpmTasks('grunt-cp');
+  grunt.loadNpmTasks('grunt-clean');
+  grunt.loadNpmTasks('grunt-connect');
 }
